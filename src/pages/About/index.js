8ec297f6f7b44484443dfc0db0a48103b5f85e55
Richard Befayt <you@example.comsprichie.rb@gmail.com>
2022-05-28 11:11:51 +0200

    Dispatch sur About

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,5 +1,6 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import { useState } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 import CurriculumItems from "../../components/CurriculumItems";
 
@@ -10,33 +11,35 @@ import meImg from '../../assets/images/apropos-img.jpg';
 const About = () => {
     const curriculums = useSelector((state) => state.curriculum.data);
 
-    const [ clickOne, setClickOne ] = useState(false);
-    const handleClickOne = () => setClickOne(!clickOne);
-    
-    const [ clickTwo, setClickTwo ] = useState(false);
-    const handleClickTwo = () => setClickTwo(!clickTwo);
+    const dispatch = useDispatch();
+    const isClick = useSelector((state) => state.curriculum.isClick)
 
-    const [ clickThree, setClickThree ] = useState(false);
-    const handleClickThree= () => setClickThree(!clickThree);
+    
 
     return (
         <div className='about'>
             <div className='about-left'>
-                <img className='about-img' src={meImg} alt="" />
+                <img className='about-img' src={meImg} alt="Photo de profil" />
             </div>
 
             <div className='about-right'>
-                <section className={clickOne ? "informations active" : "informations"} onClick={handleClickOne}>
-                    
-                    <h2 className='informations-title'>Informations</h2>
+                <section
+                    className={isClick ? "informations active" : "informations"}
+                    onClick={() => dispatch({ type: "TOGGLE_SETTINGS" })}
+                >
+
+                    <h2 className="informations-title">Informations</h2>
                     <h3 className='informations-subtitle'>Richard BEFAYT - Développeur Web Front-end</h3>
                     <p className='informations-contact'>06.35.02.11.58 - rbefayt@gmail.com</p>
                     <p className='informations-presentation'>Bonjour et bienvenue sur mon Portfolio. Passionné depuis toujours par la création, j'ai débuté ma carrière professionnelle par le cinéma. Aujourd'hui je me reconverti en tant que Développeur Web Front-End.</p>
                 </section>
 
-                <section className={clickTwo ? "formations active" : "formations"} onClick={handleClickTwo}>
+                <section
+                    className={isClick ? "formations active" : "formations"}
+                    onClick={() => dispatch({ type: "TOGGLE_SETTINGS" })}
+                >    
+                    <h2 className="formations-title">Formations et expériences professionnelles</h2>
                     
-                    <h2 className='formations-title'>Formations et expériences professionnelles</h2>
 
                     <div className='formations-container'>
                 {
@@ -47,14 +50,20 @@ const About = () => {
             </div>
                 </section>
 
-                <section className={clickThree ? "personal active" : "personal"} onClick={handleClickThree}>
-                    <h2 className='personal-title'>Loisirs et Passions</h2>
-                    <p className='personal-item'>Moto</p>
-                    <p className='personal-item'>Astronomie</p>
-                    <p className='personal-item'>Jeux Vidéo</p>
-                    <p className='personal-item'>Histoire</p>
-                    <p className='personal-item'>Voyages</p>
-                    <p className='personal-item'>Voyages</p>
+                <section
+                    className={isClick ? "personal active" : "personal"}
+                    onClick={() => dispatch({ type: "TOGGLE_SETTINGS" })}
+                >
+                    <h2 className="personal-title">Loisirs et Passions</h2>
+                    
+                    <ul className='personal-list'>
+                        <li className='personal-item'>Jeux Vidéo</li>
+                        <li className='personal-item'>Moto</li>
+                        <li className='personal-item'>Astronomie</li>
+                        <li className='personal-item'>Histoire</li>
+                        <li className='personal-item'>Voyages</li>
+                        <li className='personal-item'>Airsoft</li>
+                    </ul>
                 </section>
             </div>
         </div>
--- src/reducers/curriculum.js
+++ src/reducers/curriculum.js
@@ -2,12 +2,18 @@ import curriculumData from "../data/curriculumData";
 
 export const initialState = {
     data: curriculumData,
+    isClick: false,
 };
 
 const reducer = (state = initialState, action = {}) => {
     // console.log("reducer curriculumData", state);
     // Dans 1 reducer combiné, on a uniquement accès qu'à sa tranche de state.
     switch (action.type) {
+        case "TOGGLE_SETTINGS":
+            return {
+                ...state,
+                isClick: !state.isClick,
+            }
         default:
             return state;
     }
