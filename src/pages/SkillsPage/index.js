4334f41f6dd59459e372b0f6abf7f6fe4098ad2f
Richard Befayt <you@example.comsprichie.rb@gmail.com>
2022-08-22 14:19:53 +0200

    SkillsPage cachée

--- src/pages/SkillsPage/index.js
+++ src/pages/SkillsPage/index.js
@@ -1,38 +1,38 @@
-import { useSelector, useDispatch } from "react-redux";
+// import { useSelector, useDispatch } from "react-redux";
 
-import Elements from "./Elements";
+// import Elements from "./Elements";
 
-import './skills-page.css';
+// import './skills-page.css';
 
-const SkillsPage = () => {
-    const open = useSelector((state) => state.skills.open);
+// const SkillsPage = () => {
+//     const open = useSelector((state) => state.skills.open);
 
-    const skills = useSelector((state) => state.skills.allLanguages);
-    console.log("skills.elements :", skills.elements);
+//     const skills = useSelector((state) => state.skills.allLanguages);
+//     console.log("skills.elements :", skills.elements);
 
-    // skills.forEach((skill) => console.log(skill.elements));
-    // const elements = skills.forEach((skill) => console.log(skill.elements));
+//     // skills.forEach((skill) => console.log(skill.elements));
+//     // const elements = skills.forEach((skill) => console.log(skill.elements));
     
     
-    const dispatch = useDispatch();
+//     const dispatch = useDispatch();
 
-    return (
-        <div className='skills-page'>
-            <button
-                className='btn btn-light'
-                onClick={() => dispatch({ type: 'TOGGLE_OPEN' })}
-            >
-                Title
-                {/* {elements.name} */}
-            </button>
-            {/* <div className={`${open ? 'snippets-content open' : 'snippets-content'}`}>
-                {
-                    elements.map(
-                        (skill) => <Elements key={skill.id} {...skill} />
-                    )
-                }
-            </div> */}
-        </div>
-    );
-}
-export default SkillsPage;
+//     return (
+//         <div className='skills-page'>
+//             <button
+//                 className='btn btn-light'
+//                 onClick={() => dispatch({ type: 'TOGGLE_OPEN' })}
+//             >
+//                 Title
+//                 {/* {elements.name} */}
+//             </button>
+//             {/* <div className={`${open ? 'snippets-content open' : 'snippets-content'}`}>
+//                 {
+//                     elements.map(
+//                         (skill) => <Elements key={skill.id} {...skill} />
+//                     )
+//                 }
+//             </div> */}
+//         </div>
+//     );
+// }
+// export default SkillsPage;
